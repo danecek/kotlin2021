0fc1e4f13750a60e309ade251a2e2f9220ed3b9c
Ing. Jiří Daněček <jiri.danecek@fit.cvut.cz>
2021-09-09 10:52:12 +0200

    update

--- expr/src/main/kotlin/kotlin2021/expr/expr.kt
+++ expr/src/main/kotlin/kotlin2021/expr/expr.kt
@@ -2,34 +2,45 @@ package kotlin2021.expr
 
 interface Expr {
     val value: Int
+    val pri: Int
 }
 
 open class Num(override val value: Int) : Expr {
+    override val pri = 0
     override fun toString() = "$value"
 }
 
-enum class Op {
-    PLS {
+enum class Op(val pri: Int) {
+    PLS(2) {
         override fun toString() = '+'.toString()
     },
-    MLT {
+    MLT(1) {
         override fun toString() = '*'.toString()
     }
 }
 
 class Bin(val op: Op, val left: Expr, val right: Expr) : Expr {
-    override val value: Int
-        get() = when (op) {
-            Op.PLS -> left.value + right.value
-            Op.MLT -> left.value * right.value
-        }
+    override val pri= op.pri
+    override val value: Int = when (op) {
+        Op.PLS -> left.value + right.value
+        Op.MLT -> left.value * right.value
+    }
 
     override fun toString(): String {
-        return "$left $op $right"
+        fun String.encl() = "($this)"
+        val l = if (pri > left.pri)  left.toString() else left.toString().encl()
+        val r = if (pri > right.pri)  right.toString() else right.toString().encl()
+        return "$l $op $r"
     }
 
 }
 
+val Expr.postfix : String
+   get() = when(this) {
+       is Num -> toString()
+       is Bin -> "${left.postfix} ${right.postfix} $op"
+       else ->error("")
+   }
 
 object One : Num(1)
 object Zero : Num(0)
@@ -37,5 +48,8 @@ object Two : Num(2)
 
 fun main() {
     val e = Bin(Op.MLT, Bin(Op.PLS, One, One), Two)
+    val e2 = Bin(Op.MLT, Bin(Op.PLS, One, One), Two)
+    println(e==e2)
     println("$e = ${e.value}")
+    println(e.postfix)
 }
