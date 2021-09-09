3962e6e4f71eca3f544e1f8977db6c61cf80f6aa
Ing. Jiří Daněček <jiri.danecek@fit.cvut.cz>
2021-09-09 12:20:22 +0200

    update

---
+++ expr/src/main/kotlin/kotlin2021/expr/patt/patt.kt
@@ -0,0 +1,32 @@
+package kotlin2021.expr.patt
+
+import kotlin2021.expr.Bin
+import kotlin2021.expr.Expr
+import kotlin2021.expr.Num
+import kotlin2021.expr.*
+
+interface Patt {
+    fun match(e : Expr) : Boolean
+}
+
+object UnivPatt: Patt {
+    override fun match(e: Expr): Boolean = true
+}
+
+open class NumPatt(val value: Int) : Patt {
+    override fun match(e: Expr) = e is Num && e.value == value
+}
+object OnePatt : NumPatt(1)
+object ZeroPatt : NumPatt(0)
+
+fun Expr.optim() =
+    when {
+
+      else -> this
+    }
+
+
+fun main() {
+    val e = Bin(Op.PLS, Zero, One)
+    println(e.optim()) //     println(UnivPatt.i(One).match())
+}
