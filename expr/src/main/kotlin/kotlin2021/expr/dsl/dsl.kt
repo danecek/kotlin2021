d797a8debec80b709fcce30995ce90f1335e2fc2
Ing. Jiří Daněček <jiri.danecek@fit.cvut.cz>
2021-09-11 00:42:35 +0200

    update

--- expr/src/main/kotlin/kotlin2021/expr/dsl/dsl.kt
+++ expr/src/main/kotlin/kotlin2021/expr/dsl/dsl.kt
@@ -6,6 +6,7 @@ import kotlin2021.expr.Num
 import kotlin2021.expr.Op
 import java.util.*
 
+/*
 fun Stack<Expr>.num(value: Int): Expr {
     val n = Num(value)
     push(n)
@@ -28,26 +29,41 @@ fun Stack<Expr>.mlt(operands: Stack<Expr>.() -> Unit): Expr {
     val b = Bin(Op.MLT, l, r)
     push(b)
     return b
-}
-
-/*fun Queue<Expr>.pls(operands: Queue<Expr>.() -> Unit): Expr {
-    val r = ArrayQueue<Expr>().apply{ operands }.add( Bin(Op.PLS,  , r))
-    operands()
-    val r = pop()
-    val l = pop()
-    val b = Bin(Op.PLS, l, r)
-    push(b)
-    return b
 }*/
 
+fun Queue<Expr>.pls(operands: Queue<Expr>.() -> Unit): Expr {
+    val bin = ArrayDeque<Expr>()
+        .apply { operands() }
+        .apply { check(size == 2) }
+        .run {
+            Bin(Op.PLS, remove(), remove())
+        }
+    return apply { add(bin) }.last()
+}
+
+fun Queue<Expr>.mlt(operands: Queue<Expr>.() -> Unit): Expr {
+    val bin = ArrayDeque<Expr>()
+        .apply { operands() }
+        .apply { check(size == 2) }
+        .run {
+            Bin(Op.MLT, remove(), remove())
+        }
+    return apply { add(bin) }.last()
+}
+
+fun Queue<Expr>.num(value: Int): Expr {
+    return apply { add(Num(value)) }.last()
+}
+
 fun main() {
-    val e = Stack<Expr>().pls {
+    val e = ArrayDeque<Expr>().mlt {
         num(3)
-        pls{
-            num(5)
+        pls {
+            num(4)
             num(5)
         }
 
     }
     println(e)
 }
+
--- expr/src/main/kotlin/kotlin2021/expr/dsl/dsl2.kt
+++ expr/src/main/kotlin/kotlin2021/expr/dsl/dsl2.kt
@@ -1,13 +1,18 @@
 package kotlin2021.expr.dsl
 
-import kotlin2021.expr.*
+import kotlin2021.expr.Bin
+import kotlin2021.expr.Expr
+import kotlin2021.expr.Num
+import kotlin2021.expr.Op
 
-operator fun Expr.plus(r : Expr) = Bin(Op.PLS, this, r)
-operator fun Expr.plus(r : Int) = Bin(Op.PLS, this, Num(r))
+operator fun Expr.plus(r: Expr) = Bin(Op.PLS, this, r)
+operator fun Expr.plus(r: Int) = Bin(Op.PLS, this, Num(r))
+operator fun Expr.times(r: Expr) = Bin(Op.MLT, this, r)
+operator fun Expr.times(r: Int) = Bin(Op.MLT, this, Num(r))
 
 operator fun Int.not() = Num(this)
 
 fun main() {
-    println(!1 + 2)
+    println((!1 + 2) * 3)
 
 }
